7fa618e48d345c981ff6fd7b4a8f95123b41f65a
Alex Kwan <alexkwan089@gmail.com>
2011-04-03 12:43:53 -0400

    Adding an object oriented Hello WOrld
    Signed-off-by: Alex Kwan <alexkwan089@gmail.com>

---
+++ JavaHelloWorld/src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+	String _message;
+	public Hello() {
+		_message = "Hello World";
+	}
+	public void greet() {
+		System.out.println(_message);
+	}
+}
--- JavaHelloWorld/src/HelloWorld.java
+++ JavaHelloWorld/src/HelloWorld.java
@@ -2,9 +2,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
-		
-		System.out.println("Goosecheese: SOO OGOOD");
+		new Hello().greet();
 		
 	}
 }
