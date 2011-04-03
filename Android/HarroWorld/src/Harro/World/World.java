a32c4a8c0d4db7daadda120749b22748abaa9fd7
Alex Kwan <alexkwan089@gmail.com>
2011-04-03 17:08:37 -0400

    Adding HelloWorld(Incomplete) for both Android and Python
    Signed-off-by: Alex Kwan <alexkwan089@gmail.com>

---
+++ Android/HarroWorld/src/Harro/World/World.java
@@ -0,0 +1,13 @@
+package Harro.World;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class World extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ Python/HelloWorld/src/HelloWorld.py
@@ -0,0 +1,10 @@
+'''
+Created on Apr 3, 2011
+
+@author: alkwan
+'''
+
+v
+if __name__ == '__main__':
+    main()
+    
